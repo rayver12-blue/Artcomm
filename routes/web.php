7fff0bdcfc3b73485656f1199e8a3e92f03b2454
AShen-dead <tenzai385@gmail.com>
2025-09-15 00:05:17 +0800

    made new routes for artists

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,11 @@ Route::get('/', function () {
 Route::get('/Artists/tako', function () {
     return view('Artists.tako');
 })->name('Artists.tako');
+
+Route::get('/Artists/ashen', function () {
+    return view('Artists.ashen');
+})->name('Artists.ashen');
+
+Route::get('/Artists/artur', function () {
+    return view('Artists.artur');
+})->name('Artists.artur');
