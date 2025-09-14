d255e074f599aa95fe86264cbdddea8cf5d81c78
AShen-dead <tenzai385@gmail.com>
2025-09-15 00:00:31 +0800

    added new artist files

---
+++ resources/views/Artists/artur.blade.php
@@ -0,0 +1,35 @@
+
+<x-layout title="Artur Biography">
+<nav class="navbar navbar-expand-sm bg-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <button class="btn btn-circle"><a href="{{ url()->previous() }}" class="nav-link"><=</a></button>
+      </li>
+    </ul>
+  </div>
+</nav>
+    <body>
+  <div class="profpage" style="background-image: url('{{ asset('images/arturbg.jpg') }}');">
+    <div class="container text-center">
+      <p class="mb-2">DIGITAL ART</p>
+      <h1 class="fw-bold">LEARN TO COLOR YOUR ART</h1>
+      <h2 class="mb-3"></h2>
+      <p>ILLUSTRATOR, Artur-link</p>
+      
+      <div class="d-flex justify-content-center gap-3 mt-4">
+        <button class="btn btn-circle">⟳</button>
+        <button class="btn btn-circle">▶</button>
+        <button class="btn btn-circle">🏷</button>
+      </div>
+    </div>
+  </div>
+    <div class="container text-center">
+      <h3>About this artist</h3>
+      <p>Artist with ?? years of experience in drawing</p>
+    </div>
+    <div class="card">
+
+    </div>
+</body>
+</x-layout>
