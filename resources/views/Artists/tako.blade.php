47990edd9c496aab4f7af4295a0bb8057aa7ec7a
AShen-dead <tenzai385@gmail.com>
2025-09-28 00:07:15 +0800

    modified css

--- resources/views/Artists/tako.blade.php
+++ resources/views/Artists/tako.blade.php
@@ -1,12 +1,21 @@
 
 <x-layout title="tako Biography">
+<nav class="navbar navbar-expand-sm bg-dark">
+  <div class="container-fluid">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <button class="btn btn-circle"><a href="{{ url()->previous() }}" class="nav-link"><=</a></button>
+      </li>
+    </ul>
+  </div>
+</nav>
     <body>
-  <div class="profpage">
+  <div class="profpage" style="background-image: url('{{ asset('images/tako.jpg') }}');">
     <div class="container text-center">
       <p class="mb-2">DIGITAL ART</p>
       <h1 class="fw-bold">LEARN TO COLOR YOUR ART</h1>
       <h2 class="mb-3"></h2>
-      <p>ILLUSTRATOR, Ashen</p>
+      <p>ILLUSTRATOR, Tako</p>
       
       <div class="d-flex justify-content-center gap-3 mt-4">
         <button class="btn btn-circle">⟳</button>
@@ -15,9 +24,12 @@
       </div>
     </div>
   </div>
-  <div class="container text-center">
+    <div class="container text-center">
       <h3>About this artist</h3>
       <p>Artist with ?? years of experience in drawing</p>
+    </div>
+    <div class="card">
+
     </div>
 </body>
 </x-layout>
